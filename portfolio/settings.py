f7fe6ad5c5345265a3c1e225d4c0cd8a3b3477d0
NajlaBH-django <bhndevtools@gmail.com>
2022-01-05 16:03:50 +0000

    :wrench: Hide secret key prep for prod.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+import os
 import django_heroku
 from pathlib import Path
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'demo-to-change-and-hide-in-prod-django-insecure-ha&7zeo#6f2hp)6xd)xc1@16x+2wq-*$6gu2zg!o3ivrl=x+#w'
+SECRET_KEY = os.getenv('SECRET_KEY','demo-to-change-and-hide-in-prod-django-insecure-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
